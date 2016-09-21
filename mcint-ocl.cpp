92d51b5791dc0182fd87075823c95c5e462b5536
LinuxUser404 <nikolay.pershyn@gmail.com>
2016-09-20 23:56:26 -0600

    OpenCL platform test

--- mcint-ocl.cpp
+++ mcint-ocl.cpp
@@ -16,12 +16,12 @@ int main(int argc, char* argv[])
 	std::vector< cl::Platform > platformList;
 	cl::Platform::get(&platformList);
 	checkErr(platformList.size()!=0 ? CL_SUCCESS : -1, "cl::Platform::get");
-	std::cerr << "Platform number is: " << platformList.size() << std::endl << std::flush;
+	std::cout << "Platform number is: " << platformList.size() << std::endl << std::flush;
 	std::string platformVendor;
 	for(unsigned i = 0; i < platformList.size(); ++i)
 	{
 		platformList[i].getInfo((cl_platform_info)CL_PLATFORM_VENDOR, &platformVendor);
-		std::cerr << "Platform is by: " << platformVendor << std::endl;
+		std::cout << "Platform is by: " << platformVendor << std::endl;
 	}
 	cl_context_properties cprops[3] = {CL_CONTEXT_PLATFORM, (cl_context_properties)(platformList[0])(), 0};
 	cl::Context context(CL_DEVICE_TYPE_GPU, cprops, NULL, NULL, &err);
